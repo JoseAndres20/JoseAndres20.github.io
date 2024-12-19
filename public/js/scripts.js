57d63a9c25f98a32f9ea8ed80e22be01fd391927
Jose Andres <133692151+JoseAndres20@users.noreply.github.com>
2024-12-19 11:22:59 -0600

    Add files via upload

---
+++ public/js/scripts.js
@@ -0,0 +1,7 @@
+// Inicializa las animaciones AOS (Animation On Scroll)
+AOS.init({
+    duration: 1000,  // Duración de la animación
+    easing: 'ease-in-out',  // Efecto de la animación
+    once: true,  // La animación solo ocurre una vez
+  });
+  
